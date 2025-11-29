5a205a91ff847648882a7f2cc1502cc7ff15f021
Bick Hsu <bickhsu@gmail.com>
2025-11-29 19:01:51 +0800

    feat: add [ api/write ] to write articale from the frontend

--- backend/src/backend/__init__.py
+++ backend/src/backend/__init__.py
@@ -1,2 +1,3 @@
 def main() -> None:
-    print("Hello from backend!")
+    print("Starting Backend Server...")
+
--- backend/src/backend/app.py
+++ backend/src/backend/app.py
@@ -1,7 +1,38 @@
+# IMPORT BUILT-IN
+from pathlib import Path
+from datetime import datetime
+
+# IMPORT THIRD-PARTY
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+ARTICLES_DIR = Path("articles")
+ARTICLES_DIR.mkdir(exist_ok=True)
+
+
+class Article(BaseModel):
+    title: str
+    content: str
+
 
 @app.get("/")
 def home():
     return {"message": "Hello, Bick! FastAPI is running."}
+
+
+@app.post("/api/write")
+def write_article(article: Article):
+    """Recive title and content then save it into txt file"""
+    filename = f"{article.title}_{datetime.now().strftime("%Y%m%d_%H%M%S")}.txt"
+    filepath = ARTICLES_DIR / filename
+
+    with open(filepath, "w", encoding="utf-8") as f:
+        f.write(article.content)
+
+    return {
+        "status": "ok",
+        "saved_to": filepath
+    }
