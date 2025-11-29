3b13df42b4c34c73d45d4888d98f52a276fe4f94
Bick Hsu <bickhsu@gmail.com>
2025-11-29 21:42:42 +0800

    style: uniform style

--- backend/src/backend/app.py
+++ backend/src/backend/app.py
@@ -29,17 +29,18 @@ def is_safe_filepath(filepath: Path) -> bool:
 
 @app.get("/")
 def home():
-    return {"message": "Hello, Bick! FastAPI is running."}
+    return {
+        "message": "Hello, Bick! FastAPI is running."
+    }
 
 
 @app.post("/api/write")
 def write_article(article: Article):
-    """Recive title and content then save it into txt file"""
+    """Receive title and content then save it into .md file"""
     filename = f"{article.title}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.md"
     filepath = ARTICLES_DIR / filename
 
-    with open(filepath, "w", encoding="utf-8") as f:
-        f.write(article.content)
+    filepath.write_text(article.content, encoding="utf-8")
 
     return {
         "status": "ok",
@@ -60,14 +61,12 @@ def list_all_article():
 
 @app.get("/api/read/{filename}")
 def read_article(filename: str):
-    """Read article by filename"""
-
-    # prevent injection
+    """Read article by filename (full name, including .md)"""
     if not is_safe_filename(filename):
         raise HTTPException(status_code=400, detail="Invalid filename")
 
-    # normalize path
     filepath = (ARTICLES_DIR / filename).resolve()
+
     if not is_safe_filepath(filepath):
         raise HTTPException(status_code=400, detail="Invalid filename")
 
@@ -89,13 +88,11 @@ def read_article(filename: str):
 @app.delete("/api/delete/{filename}")
 def delete_article(filename: str):
     """Delete article by filename"""
-
-    # prevent injection
     if not is_safe_filename(filename):
         raise HTTPException(status_code=400, detail="Invalid filename")
 
-    # normalize path
     filepath = (ARTICLES_DIR / filename).resolve()
+    
     if not is_safe_filepath(filepath):
         raise HTTPException(status_code=400, detail="Invalid filename")
 
