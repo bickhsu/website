14b01bf5be6059653dda2bbaac6dc8ec33a1f824
Bick Hsu <bickhsu@gmail.com>
2025-12-04 02:09:13 +0800

    feat: add frontend basic structure

---
+++ frontend/main.js
@@ -0,0 +1,57 @@
+const API_BASE = "http://127.0.0.1:8000"
+
+async function submitArticle() {
+    const title = document.getElementById("title").value;
+    const content = document.getElementById("content").value;
+
+    await fetch(`${API_BASE}/api/write`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title, content })
+    });
+
+    alert("Article Saved!!")
+    loadArticles();
+}
+
+
+async function loadArticles() {
+    const res = await fetch(`${API_BASE}/api/list`);
+    const data = await res.json();
+
+    const list = document.getElementById("articleList");
+    list.innerHTML = "";
+
+    data.articles.forEach(name => {
+        const li = document.createElement("li")
+
+        li.innerHTML = `
+            <strong>${name}</strong>
+            <button onclick="readArticle('${name}')">Read</button>
+            <button onclick="deleteArticle('${name}')">Delete</button>
+        `;
+
+        list.appendChild(li);
+    })
+}
+
+
+async function readArticle(filename) {
+    const res = await fetch(`${API_BASE}/api/read/${filename}`);
+    const data = await res.json();
+
+    alert("Content:\n\n + data.content");
+}
+
+
+async function deleteArticle(filename) {
+    await fetch(`${API_BASE}/api/delete/${filename}`, {
+        method: "DELETE"
+    });
+    
+    alert("Delete: " + filename);
+    loadArticles();
+}
+
+document.getElementById("submitBtn").onclick = submitArticle;
+loadArticles();
