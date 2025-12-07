33493e3ad22222965479eda11c6beac91ce365d4
Bick Hsu <bickhsu@gmail.com>
2025-12-07 20:10:23 +0800

    feat: init database models

---
+++ backend/database/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Text
+from .database import Base
+
+
+class Article(Base):
+    __tablename__ = "articles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    content = Column(Text)
