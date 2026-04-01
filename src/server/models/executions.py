756b4dc134f3eee43c8a00c1546431f1bb74feb9
Bick Hsu <bickhsu@gmail.com>
2026-04-02 01:19:57 +0800

    feat: add domain column in execute unit

--- src/server/models/executions.py
+++ src/server/models/executions.py
@@ -8,6 +8,7 @@ class ExecutionUnit(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     title = Column(String(150), nullable=False, server_default="Untitled Action")
+    domain = Column(String(50), server_default="General")
     status = Column(String(20), server_default="To-Do")
     problem_statement = Column(Text, nullable=False)
     value_delivered = Column(Text)
