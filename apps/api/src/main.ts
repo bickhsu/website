4ed4935fe71947ae3e5bc72b101fb36568c52d03
bickhsu <bickhsu@gmail.com>
2026-04-16 18:10:42 +0800

    feat(main): set global prefix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,7 +6,7 @@ import { GlobalExceptionsFilter } from './common/filters/global-exceptions.filte
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
