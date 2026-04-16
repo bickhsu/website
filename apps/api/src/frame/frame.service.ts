2e85f065b5e5fd8e372d1be3fe485f03ef7bf934
bickhsu <bickhsu@gmail.com>
2026-04-16 16:34:17 +0800

    refactor(frame): remove try-catch logic

--- apps/api/src/frame/frame.service.ts
+++ apps/api/src/frame/frame.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreateFrameDto } from './dto/create-frame.dto';
 import { UpdateFrameDto } from './dto/update-frame.dto';
 import { PrismaService } from '../database/prisma.service';
@@ -8,41 +8,27 @@ export class FrameService {
   constructor(private readonly prisma: PrismaService) { }
 
   async create(createFrameDto: CreateFrameDto) {
-    try {
-      return await this.prisma.frame.create({
-        data: createFrameDto,
-      });
-    } catch (error) {
-      throw new InternalServerErrorException('Failed to create the frame.');
-    }
+    return this.prisma.frame.create({
+      data: createFrameDto,
+    });
   }
 
   async update(id: string, updateFrameDto: UpdateFrameDto) {
-    try {
-      return await this.prisma.frame.update({
-        where: { id },
-        data: updateFrameDto,
-      });
-    } catch (error) {
-      if (error.code === 'P2025') {
-        throw new NotFoundException(`Frame with ID ${id} not found`);
-      }
-      throw new InternalServerErrorException('Failed to update the frame.');
-    }
+    return this.prisma.frame.update({
+      where: { id },
+      data: updateFrameDto,
+    });
   }
 
   async remove(id: string) {
-    try {
-      const { count } = await this.prisma.frame.deleteMany({
-        where: { id },
-      });
-      return {
-        id,
-        deleted: count > 0,
-        message: count > 0 ? 'Deleted successfully' : 'Already non-existent',
-      }
-    } catch (error) {
-      throw new InternalServerErrorException('Database execution failed.');
-    }
+    const { count } = await this.prisma.frame.deleteMany({
+      where: { id },
+    });
+
+    return {
+      id,
+      deleted: count > 0,
+      message: count > 0 ? 'Deleted successfully' : 'Already non-existent',
+    };
   }
 }
