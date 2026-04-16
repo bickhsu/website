f400754bd3b5615376e8d3661c89457b2ef1b519
bickhsu <bickhsu@gmail.com>
2026-04-16 16:29:08 +0800

    feat(filters): register custom filters

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,8 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaClientExceptionFilter } from './common/filters/prisma-client-exception.filter';
+import { GlobalExceptionsFilter } from './common/filters/global-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +13,14 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
       transform: true,
     }),
+  );
+
+  // global filters must be registered before specific filters
+  // Because the filters are executed in the reverse order they are registered
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(
+    new GlobalExceptionsFilter(httpAdapterHost),
+    new PrismaClientExceptionFilter(),
   );
 
   await app.listen(process.env.PORT ?? 3000);
