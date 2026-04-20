bdc9bdb1a022192854db023ab931988b67969ed5
Bick Hsu <bickhsu@gmail.com>
2026-04-20 17:19:47 +0800

    feat(web): setup API global config

---
+++ apps/web/src/config/api.ts
@@ -0,0 +1,10 @@
+/**
+ * API Global Config
+ */
+export const API_BASE_URL = 'http://localhost:3000/api/v1';
+
+export const ENDPOINTS = {
+  SEQUENCES: `${API_BASE_URL}/sequence`,
+  KEYFRAMES: `${API_BASE_URL}/keyframe`,
+  UPLOAD: `${API_BASE_URL}/upload/image`,
+};
